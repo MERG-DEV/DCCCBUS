dfbef165c937bb6570f8819078cf4f396b2251e4
Christopher Francis White <whitecf69@gmail.com>
2025-06-05 13:51:22 +0100

    Added cbuslib, latest refactor, sub module.

--- dcccan.asm
+++ dcccan.asm
@@ -21,13 +21,13 @@
 ;**********************************************************************
 ;                                                                     *
 ;                             +---+ +---+                             *
-;                        !MCLR| 1 |_| 28|RB7                          *
-;                          RA0| 2     27|RB6                          *
+;                        !MCLR| 1 |_| 28|RB7 -> Green LED (SLiM)      *
+;                          RA0| 2     27|RB6 -> Yellow LED (FLiM)     *
 ;                          RA1| 3     26|RB5                          *
 ;                          RA2| 4     25|RB4                          *
-;                          RA3| 5     24|RB3                          *
-;                          RA4| 6     23|RB2                          *
-;                          RA5| 7     22|RB1                          *
+;                          RA3| 5     24|CANRX <-                     *
+;                          RA4| 6     23|CANTX ->                     *
+;                 Setup -> RA5| 7     22|RB1                          *
 ;                    0V -> VSS| 8     21|RB0                          *
 ;                          RA7| 9     20|VDD <- +5V                   *
 ;                          RA6|10     19|VSS <- 0V                    *
@@ -44,27 +44,37 @@
 ; Include and configuration directives                                *
 ;**********************************************************************
 
-#include <p18f2480.inc>
+  include <p18f2480.inc>
 
   CONFIG  FCMEN = OFF, OSC = HSPLL, IESO = OFF
-  CONFIG  PWRT = ON,BOREN = BOHW, BORV=0
-  CONFIG  WDT=OFF
+  CONFIG  PWRT = ON, BOREN = BOHW, BORV = 0
+  CONFIG  WDT = OFF
   CONFIG  MCLRE = ON
   CONFIG  LPT1OSC = OFF, PBADEN = OFF
   CONFIG  DEBUG = OFF
-  CONFIG  XINST = OFF,LVP = OFF,STVREN = ON,CP0 = OFF
-  CONFIG  CP1 = OFF, CPB = OFF, CPD = OFF,WRT0 = OFF,WRT1 = OFF, WRTB = OFF
-  CONFIG  WRTC = OFF,WRTD = OFF, EBTR0 = OFF, EBTR1 = OFF, EBTRB = OFF
+  CONFIG  XINST = OFF, LVP = OFF, STVREN = ON
+  CONFIG  CP0 = OFF, CP1 = OFF, CPB = OFF, CPD = OFF
+  CONFIG  WRT0 = OFF, WRT1 = OFF, WRTB = OFF, WRTC = OFF, WRTD = OFF
+  CONFIG  EBTR0 = OFF, EBTR1 = OFF, EBTRB = OFF
 
 ; '__CONFIG' directive is used to embed configuration data within .asm file.
 ; The lables following the directive are located in the respective .inc file.
 ; See respective data sheet for additional information on configuration word.
 ; Selection is:
 
+; Processor uses 4 MHz resonator with HSPLL to give a clock of 16MHz
+
 
 ;**********************************************************************
 ; Constant definitions                                                *
 ;**********************************************************************
+
+  include "cbuslib/cbusdefs.inc"
+
+RESET_VECTOR               equ  0x0800
+
+
+  include   "cbuslib/boot_loader.inc"
 
 
 ;**********************************************************************
@@ -74,6 +84,18 @@
 ;**********************************************************************
 ; EEPROM initialisation                                               *
 ;**********************************************************************
+
+
+;******************************************************************************
+;   Low priority interrupt.
+
+low_priority_interrupt_routine
+
+
+;******************************************************************************
+;   High priority interrupt.
+
+high_priority_interrupt_routine
 
 
     end
